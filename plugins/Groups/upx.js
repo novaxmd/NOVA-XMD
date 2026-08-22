8c4094e8ba7780127aa014fbd792328a74d5dbfd
Bmb Tech <hanchobmb@gmail.com>
2026-08-22 04:48:54 +0300

    Update upx.js

--- plugins/Groups/upx.js
+++ plugins/Groups/upx.js
@@ -1,7 +1,7 @@
 import https from 'https';
 
 const GH_OWNER = 'novaxmd';
-const GH_REPO  = 'NOVA-XMD;
+const GH_REPO  = 'NOVA-XMD';
 const GH_BRANCH = 'main';
 const GH_ASSET_DIR = 'assets/reactions';
 
